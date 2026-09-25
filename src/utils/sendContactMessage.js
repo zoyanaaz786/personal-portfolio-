9a67e47e6883decc8b75531f33e7aeb3278b35f3
zoyanaaz786 <zoyanaazm2407@gmail.com>
2026-09-25 22:18:16 +0530

    Configure EmailJS

--- src/utils/sendContactMessage.js
+++ src/utils/sendContactMessage.js
@@ -7,23 +7,26 @@
  * of these (both are drop-in - just fill in your own IDs/keys):
  *
  * --- EmailJS -----------------------------------------------------------
- *   mport emailjs from '@emailjs/browser'
+ *   ```js
+import emailjs from '@emailjs/browser'
 
 export async function sendContactMessage(form) {
-  const result = await emailjs.send(
+  await emailjs.send(
     import.meta.env.VITE_EMAILJS_SERVICE_ID,
     import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
     {
-      from_name: form.Name,
-      from_email: form.Email,
-      subject: form.Subject,
-      message: form.Message,
+      from_name: form.name,
+      from_email: form.email,
+      subject: form.subject,
+      message: form.message,
     },
-    import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+    {
+      publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+    }
   )
-
-  return result
 }
+```
+
  *
  * --- Formspree -----------------------------------------------------------
  *   export async function sendContactMessage(form) {
